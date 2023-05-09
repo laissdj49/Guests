e215829fdd7c051e2ee9f4a364103776cc3aa3e4
laissdj49 <laissdj49@gmail.com>
2023-05-09 21:58:20 +0200

    Room implamentation

--- app/src/main/java/com/lais/study/model/GuestModel.kt
+++ app/src/main/java/com/lais/study/model/GuestModel.kt
@@ -1,3 +1,26 @@
 package com.lais.study.model
 
-data class GuestModel(val id: Int, var name: String, var presence: Boolean)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+/**
+ * o entity tem a capacidade de perceber que o GuestModel vai ser
+ * persistido no banco de dados e vai conseguir criar, temos com ele
+ * as colunas e os tipos das entidades.
+ */
+@Entity(tableName = "Guest")
+class GuestModel{
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0
+
+    @ColumnInfo(name = "name")
+    var name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = false
+
+}
